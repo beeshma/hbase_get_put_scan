243a6ab7010a56ce3b14da47e5332a473433d5c1
beeshma <beeshma48@gmail.com>
2017-07-18 20:11:25 -0600

    Initial commit

---
+++ src/main/java/com/hbase/simple_get/App.java
@@ -0,0 +1,15 @@
+package com.hbase.simple_get;
+
+/**
+ * Hello world!
+ *
+ *hello for hbase
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
